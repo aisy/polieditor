5bd39cd24e925d78acf5e7b786721eccc7e41643
ffadilaputra <i.fadilaputra@gmail.com>
2017-07-06 10:05:57 +0700

    Layouting Editor

--- application/views/latihan.php
+++ application/views/latihan.php
@@ -1,33 +1,19 @@
-<!-- Primary Style Header -->
 <?php $this->load->view('partials/mdb-header') ?>
-<!-- /.Primary Style Header -->
+<?php $this->load->view('partials/mhs_navbar') ?>
 
-
-
-<div class="wrapper">
-  <div class="outer">
-    <div class="row">
-      <!-- <div class="col-lg-12"> -->
-        <div class="jumbotron">
-          <h1 class="h1-responsive">Hello, world!</h1>
-          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr class="my-2">
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p class="lead">
-            <a class="btn btn-primary btn-lg" role="button">Learn more</a>
-          </p>
-        </div>
-      <!-- </div> -->
-      <div class="col-sm-6 sp toolbar tab-pane" id="htmPane">
+<br><br><br>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-4">
+      <div class="sp toolbar tab-pane" id="htmPane">
         <div class="pane-label">html</div>
         <div class="inner" id="editor"></div>
-      </div>
-      <div class="col-sm-6 result" id="return">
-
-      </div>
-    </div><!--/row-->
-  </div><!--/outer-->
-</div><!--/wrapper-->
-
+      </div>  
+    </div>
+    <div class="col-md-4" id="return">
+    </div>
+  </div>
+</div>
 
 <?php $this->load->view('partials/mdb-footer') ?>
